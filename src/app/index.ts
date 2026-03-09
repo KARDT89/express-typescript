d2af4d27a75fbb7b98504e0030d4f1b4f16c562d
dt89 <tamalsarkartwoooo@gmail.com>
2026-03-09 09:03:44 +0000

    add: simple routes

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,16 @@
-import express from 'express'
-import type { Application } from 'express'
+import express from "express";
+import type { Application } from "express";
 
+export function createServerApplication(): Application {
+    const app = express();
 
-export function createServerApplication(): Application{
-    const app = express()
-    return app
+    app.get("/", (req, res) => {
+        return res.json({ message: "Hello ji" });
+    });
+    
+    app.get("/hello", (req, res) => {
+        return res.json({ message: "bye" });
+    });
+
+    return app;
 }
